c6f1091e7c24d1b4a6fe48a877c6ee9fc81f9fcc
Igor Zheglov <xeltan@mail.ru>
2022-12-02 09:07:48 +0300

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun getElvesCalSums(input: List<String>): List<Int> =
+        mutableListOf<Int>().apply {
+            var currentCalSum = 0
+            input.forEachIndexed { index, element ->
+                when {
+                    element.isBlank() -> {
+                        this.add(currentCalSum)
+                        currentCalSum = 0
+                    }
+
+                    index == input.size - 1 -> {
+                        currentCalSum += element.toInt()
+                        this.add(currentCalSum)
+                    }
+
+                    else -> currentCalSum += element.toInt()
+                }
+            }
+        }
+
+    fun part1(input: List<String>): Int = getElvesCalSums(input).max()
+
+    fun part2(input: List<String>): Int = getElvesCalSums(input).sorted().takeLast(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
